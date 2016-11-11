7352af835b0b856f98e4ef4222592b3a8e46644c
J1aDong <991383877@qq.com>
2016-11-11 21:33:00 +0800

    添加了koa-body用于解析post请求

--- app.js
+++ app.js
@@ -2,27 +2,23 @@
 const Koa = require('koa');
 const app = new Koa();
 const router = require('koa-router')();
-
-app.use(async(ctx, next) =>
-{
-    const start = new Date();
-    await next();
-    const ms = new Date() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-});
+const koaBody = require('koa-body')();
 
 router.get('/users/login/:userName', async(ctx, next) =>
 {
-    const user = await "Hello,router-->"+ctx.params.userName;
+    const user = "Hello,router-->" + ctx.params.userName;
     ctx.body = user;
 });
+
+router.post('/users/register', koaBody, async(ctx, next) =>
+{
+    console.log(ctx.request.body);
+    ctx.body = ctx.request.body;
+});
+
 
 app.use(router.routes())
     .use(router.allowedMethods());
 
-app.use(ctx =>
-{
-    ctx.body = 'Hello Koa555';
-});
 
 app.listen(3000);
