ce9a9c8993e9a6b4426de00bfbb152230f3c6254
J1aDong <991383877@qq.com>
2016-11-12 21:34:07 +0800

    添加了mongoose的支持

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ const app = new Koa();
 const router = require('koa-router')();
 const koaBody = require('koa-body')();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/koa2');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+// 用户模型
+var userSchema = new Schema({
+    uid: String, // 用户 id
+    nick: String, // 用户昵称
+    pwd: String, // 用户密码
+    project: [{
+        type: ObjectId,
+        ref: 'Project'
+    }] // 这个用户对应的作品
+});
+
+// 建立Model
+
+var User = mongoose.model('User', userSchema);
+
+var user = {
+    uid: '545183867',
+    nick: '哈哈哈'
+};
+
+var newUser = new User(user);
+console.log(newUser.nick);
+newUser.save();
+
 router.get('/users/login/:userName', async(ctx, next) =>
 {
     const user = "Hello,router-->" + ctx.params.userName;
